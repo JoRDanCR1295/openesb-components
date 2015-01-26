1dfa38d24bd97f915bfef3cf7c686319d18803ee
David BRASSELY <brasseld@gmail.com>
2015-01-26 09:13:16 +0100

    Remove unused imports

--- ojc-core/restbc/jbiadapter/src/com/sun/jbi/restbc/jbiadapter/inbound/ServiceUnitResourceConfig.java
+++ ojc-core/restbc/jbiadapter/src/com/sun/jbi/restbc/jbiadapter/inbound/ServiceUnitResourceConfig.java
@@ -12,7 +12,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.ext.Provider;
 
 import com.sun.jersey.api.core.DefaultResourceConfig;
-import com.sun.jersey.server.impl.container.config.AnnotatedClassScanner;
 
 /**
  * ServiceUnitResourceConfig.java
